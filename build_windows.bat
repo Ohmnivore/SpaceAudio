86867b2e207a84b3747061c478528a3f4896e63a
Ohmnivore <markbeiline@gmail.com>
2015-11-14 00:39:11 -0500

    more automated windows building

--- build_windows.bat
+++ build_windows.bat
@@ -1,10 +1,10 @@
-@echo off
-REM pyinstaller -F -n SpaceAudio -i assets/icon.ico --noconsole src/main.py
-pyinstaller -n SpaceAudio -i assets/icon.ico --noconsole --distpath=SpaceAudioWin32 src/main.py
-REM -robocopy assets dist\assets /E /NFL /NDL /NJH /NJS /nc /ns /np
-REM -robocopy ffmpeg dist\ffmpeg /E /NFL /NDL /NJH /NJS /nc /ns /np
-REM -robocopy . dist gnu_gpl_two /E /NFL /NDL /NJH /NJS /nc /ns /np
-REM -robocopy . dist gnu_lgpl_two_point_one /E /NFL /NDL /NJH /NJS /nc /ns /np
-REM -robocopy . dist LICENSE.md /E /NFL /NDL /NJH /NJS /nc /ns /np
-REM -robocopy . dist README.html /E /NFL /NDL /NJH /NJS /nc /ns /np
-REM -robocopy . dist README.md /NFL /NDL /NJH /NJS /nc /ns /np
+REM @echo off
+rmdir /s SpaceAudioWin32
+pyinstaller -n SpaceAudio -i assets/icon.ico --noconsole --distpath=SpaceAudioWin32 --workpath=build --specpath=build src/main.py
+robocopy assets SpaceAudioWin32\assets /E
+robocopy src SpaceAudioWin32\SpaceAudio avbin.dll
+robocopy . SpaceAudioWin32 gnu_gpl_two
+robocopy . SpaceAudioWin32 gnu_lgpl_two_point_one
+robocopy . SpaceAudioWin32 LICENSE.md
+robocopy . SpaceAudioWin32 README.html
+robocopy . SpaceAudioWin32 README.md
