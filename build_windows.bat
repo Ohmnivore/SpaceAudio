8946e740baf4b828dec23c967e0a3e35dc24d19d
Ohmnivore <markbeiline@gmail.com>
2015-11-14 12:16:00 -0500

    Improved windows build script

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,5 @@
 REM @echo off
+rm SpaceAudioWin32.zip
 rmdir /s SpaceAudioWin32
 pyinstaller -n SpaceAudio -i assets/icon.ico --noconsole --distpath=SpaceAudioWin32 --workpath=build --specpath=build src/main.py
 robocopy assets SpaceAudioWin32\assets /E
@@ -8,3 +9,4 @@ robocopy . SpaceAudioWin32 gnu_lgpl_two_point_one
 robocopy . SpaceAudioWin32 LICENSE.md
 robocopy . SpaceAudioWin32 README.html
 robocopy . SpaceAudioWin32 README.md
+7za a -tzip SpaceAudioWin32.zip SpaceAudioWin32
